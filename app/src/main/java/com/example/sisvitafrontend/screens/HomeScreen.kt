5e07eba6cd9840ecd0afe73289f1a5e3a4ffb70b
BrandonVera10 <brandon.vera@unmsm.edu.oe>
2024-07-05 14:30:47 -0500

    add screens menu

---
+++ app/src/main/java/com/example/sisvitafrontend/screens/HomeScreen.kt
@@ -0,0 +1,110 @@
+package com.example.sisvitafrontend.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.sisvitafrontend.R
+import com.example.sisvitafrontend.screens.register.RegisterScreen
+import com.example.sisvitafrontend.ui.components.Background
+import com.example.sisvitafrontend.ui.components.CustomHeader
+import com.example.sisvitafrontend.ui.components.TextButton
+import com.example.sisvitafrontend.screens.login.LoginScreen
+
+@Composable
+fun HomeScreen(
+    navController: NavController
+) {
+    val showScreen = remember { mutableIntStateOf(R.string.login) }
+
+    Background()
+    Column(
+        modifier = Modifier
+            .statusBarsPadding()
+            .verticalScroll(rememberScrollState())
+            .safeDrawingPadding()
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        CustomHeader()
+        Spacer(modifier = Modifier.height(16.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(1f)
+                .padding(20.dp)
+                .background(
+                    colorResource(id = R.color.light_green_100), shape = RoundedCornerShape(12.dp)
+                )
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(1f),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                RowButtons(
+                    showScreen = {
+                        showScreen.intValue = it
+                    }, showScreen.intValue
+                )
+                if (showScreen.intValue == R.string.login) LoginScreen(
+                    navController = navController, showScreen = showScreen
+                )
+                else RegisterScreen(
+                    showScreen = showScreen
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun RowButtons(showScreen: (Int) -> Unit, value: Int) {
+    Row(
+        modifier = Modifier
+            .padding(vertical = 16.dp)
+            .background(
+                colorResource(id = R.color.light_green_300), shape = RoundedCornerShape(12.dp)
+            )
+    ) {
+        TextButton(text = R.string.login,
+            textColor = R.color.black_900,
+            shape = 12,
+            color = if (value == R.string.login) R.color.light_green_500 else R.color.light_green_300,
+            size = DpSize(150.dp, 80.dp),
+            textSize = 17,
+            onClick = {
+                showScreen(R.string.login)
+            })
+        Spacer(modifier = Modifier.width(1.dp))
+        TextButton(text = R.string.register,
+            textColor = R.color.black_900,
+            shape = 12,
+            color = if (value == R.string.login) R.color.light_green_300 else R.color.light_green_500,
+            size = DpSize(150.dp, 80.dp),
+            textSize = 17,
+            onClick = {
+                showScreen(R.string.register)
+            })
+    }
+}
---
+++ app/src/main/java/com/example/sisvitafrontend/screens/MenuPatientScreen.kt
@@ -0,0 +1,81 @@
+package com.example.sisvitafrontend.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.sisvitafrontend.R
+import com.example.sisvitafrontend.ui.components.Background
+import com.example.sisvitafrontend.ui.components.ImagenButton
+import com.example.sisvitafrontend.ui.components.CustomHeader
+import com.example.sisvitafrontend.config.navigation.Screen
+
+@Composable
+fun MenuPatientScreen(navController: NavController) {
+    Background()
+    Column(
+        modifier = Modifier
+            .statusBarsPadding()
+            .verticalScroll(rememberScrollState())
+            .safeDrawingPadding()
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ){
+        CustomHeader()
+        Spacer(modifier = Modifier.height(16.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(1f)
+                .padding(20.dp)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(1f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                ImagenButton(
+                    text = R.string.take_test,
+                    color = R.color.light_green_300,
+                    textColor = R.color.black_900,
+                    shape = 12,
+                    size = DpSize(width = 300.dp, height = 80.dp),
+                    image = R.drawable.test,
+                    imageSize = 40,
+                    textSize = 20,
+                    onClick = {
+                        navController.navigate(Screen.MenuTestScreen.route)
+                    }
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                ImagenButton(
+                    text = R.string.check_results,
+                    color = R.color.light_green_300,
+                    textColor = R.color.black_900,
+                    shape = 12,
+                    size = DpSize(width = 300.dp, height = 80.dp),
+                    image = R.drawable.sociology,
+                    imageSize = 40,
+                    textSize = 20,
+                    onClick = {
+                        navController.navigate(Screen.ConsultarResultadosScreen.route)
+                    }
+                )
+            }
+        }
+    }
+}
